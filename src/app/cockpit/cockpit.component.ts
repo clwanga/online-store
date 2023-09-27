37ceed988f921781f00bdbb6b498467077109d50
Charles Lwanga <clwanga1095@gmail.com>
2023-09-27 12:17:58 +0300

    worked on view encapsulation

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -9,23 +9,23 @@ export class CockpitComponent {
   //properties
   @Output() blueprintCreated = new EventEmitter<{serverName:string, serverContent:string}>();
   @Output() serverCreated = new EventEmitter<{serverName:string, serverContent:string}>();
-  newServerName = "";
-  // newServerContent = "";
+
+  @ViewChild("serverName",{static: true}) serverName: ElementRef;
   @ViewChild("serverContent",{static:true}) serverContent: ElementRef;
   
   //methods
   onAddServer(nameInput: HTMLInputElement){
     this.serverCreated.emit({
-      serverName: nameInput.value,
+      serverName: this.serverName.nativeElement.value,
       serverContent: this.serverContent.nativeElement.value
     });
   }
 
-  onAddBlueprint(){
-    // this.blueprintCreated.emit({
-    //   serverName: this.newServerName,
-    //   serverContent: this.newServerContent
-    // });
+  onAddBlueprint(contentInput: HTMLInputElement){
+    this.blueprintCreated.emit({
+      serverName: this.serverName.nativeElement.value,
+      serverContent: this.serverContent.nativeElement.value
+    });
   }
 
 }
